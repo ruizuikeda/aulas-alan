4fb026699d591068f93d2273a793d46cb657bf5d
Ikeda <ruizuikeda@gmail.com>
2018-04-18 15:36:05 -0300

    sessions

--- home.php
+++ home.php
@@ -1,1 +1,16 @@
-ola
+<?php
+session_start();
+$nome = $_SESSION['logado_nome'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Olá, <?php echo $nome; ?></h1>
+    </body>
+</html>
--- valida.php
+++ valida.php
@@ -1,20 +1,35 @@
 <?php
+session_start();
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 $falso = 1;
 
+
+
 if ($username == 'ikeda' && $password == '123'){
 
     $falso =0;
-    header('location:home.php?');
 
-}
-else {
+    $_SESSION['logado_nome'] = 'ikeda';
+    $_SESSION['logado_cpf'] = '366.582.098-71';
+
+    header('location:home.php');
+
+} else if ($username == 'alan' && $password == '321'){
+    $falso =0;
+
+    $_SESSION['logado_nome'] = 'alan';
+    $_SESSION['logado_cpf'] = '123.123.123-32';
+
+    header('location:home.php');
+} else {
 
     $falso = 1;
     header('location:index.php?falso='. $falso);
 
 }
+
 
 
 exit;
